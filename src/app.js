f2386976992537c101fd052c2761d1b33f0c7e30
divyaarora1808 <f20212999@goa.bits-pilani.ac.in>
2022-10-10 06:46:43 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { useState,useEffect } from 'react'
+import PageTitle from './components/PageTitle';
+import Apod from './components/Apod/Apod';
+import ApodControls from './components/ApodControls/ApodControls';
+import fixDateFormat from './components/ApodControls/fixDateFormat';
+
+//import API_KEY from './secret.js'
+
+function App() {
+  const today = new Date();
+  let fixedtoday = fixDateFormat(`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`)
+  const [date,setDate] = useState(fixedtoday)
+  const [ApodData,setApodData] = useState({title:'loading...'})
+  useEffect(()=>{
+      async function fetchData() {
+      const data = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${2nB9JEqwarYSGzyK5kR72jpes0eHKck7xo4ZZoW6}&date=${date}`)
+      const apodData = await data.json()
+      setApodData(apodData)
+    }
+    fetchData()
+  },[date])
+
+  const dateChangeHandler = (newDate) => {
+    setDate(newDate);
+  }
+  
+  return (
+    <div>
+      <PageTitle />
+      <ApodControls currentDate={date} today={fixedtoday} setDate={dateChangeHandler}/>
+      <Apod apodData={ApodData}/>
+    </div>
+  );
+}
+
+export default App;
